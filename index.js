12c03b32041f94bdf3e3fd5b84fe6de0099f9f5a
Mudassir Ahmad <ma6386731@gmail.com>
2026-05-19 10:00:06 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// let name = 'mudassir ahmad';
+// let newarr = [];
+// name.split(' ').forEach(element => {
+    
+//     let reverse = element.split("").reverse().join('')
+//     newarr.push(reverse)
+// });
+
+// console.log(newarr);
+
+
