e63061317759f8da2d24a95b973e4d7a60651e00
Mudassir-ahmad551080 <mudassir@example.com>
2026-05-30 09:03:36 +0500

    Add str.js with palindrome and alphabet order helper functions

---
+++ str.js
@@ -0,0 +1,21 @@
+// function palchecker(str){
+//     let another = str.split('').reverse().join('');
+//     if(str === another){
+//         console.log('It is a palindrome');
+//         return true;
+//     }
+//     else{
+//         console.log('It is not a palindrome');
+//         return false;
+//     }
+// }
+
+// console.log(palchecker('loop'))
+
+// write a javascript function that takes a string as input and returns the string into alphabitical order
+
+// function alphabetOrder(str){
+//     return str.split('').sort().join('')
+// }
+
+// console.log(alphabetOrder('cba'))
